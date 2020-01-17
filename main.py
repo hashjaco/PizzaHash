1b91d18563bbfaebcc48ca53c50c408e09ed33b4
hashjaco <hashjaco@github.com>
2020-01-16 22:45:45 -0800

    findCombination method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 # data sets ordered by size, ascending
 set_a = open('data/a_example.in', 'r', encoding='utf-8')
@@ -10,11 +11,37 @@ set_e = open('data/e_also_big.in', 'r', encoding='utf-8')
 def getData(file):
     max_slices, num_types = file.readline().replace("\n", "").split(" ")
     pizza_weights = file.readline().replace("\n", "").split(" ")
-
     return max_slices, num_types, pizza_weights
 
 
+def buildPizzaDictionary(weight_list: []):
+    size = len(weight_list)
+    assert size > 0
+    pizza_dict = {}
+
+    try:
+        for x in range(size):
+            pizza_dict.update({f'pizza {x + 1}' : int(weight_list.pop(0))})
+
+        return pizza_dict
+    except IndexError:
+        print("index is out of range")
+
+
+
+def findCombination(max_slices, pizzas: {}):
+    num_pizzas = len(pizzas)
+    assert num_pizzas > 0 and max_slices > 0
+    knapsack_matrix = np.array([[], []])
+    for row in range(max_slices):
+        for column in range(num_pizzas):
+
+    return knapsack_matrix[:, max_slices]
+
+
 max_slices, num_types, pizza_weights = getData(set_a)
+pizza_dictionary = buildPizzaDictionary(pizza_weights)
 print(max_slices)
 print(num_types)
 print(pizza_weights)
+print(pizza_dictionary)
